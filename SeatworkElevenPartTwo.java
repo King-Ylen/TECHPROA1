77c1d30831de82d8077d6ad78b7c6eb1f5208961
King Ylen <kingylen.scc@gmail.com>
2025-09-08 02:00:44 +0000

    done

---
+++ SeatworkElevenPartTwo.java
@@ -0,0 +1,11 @@
+
+public class SeatworkElevenPartTwo {
+    public static void main(String[] args) {
+        System.out.println("SEATWORKELEVENPART2");
+        System.out.println();
+
+        int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
+        myNumbers[1][2] = 9;
+        System.out.println(myNumbers[1][2]);
+    }
+}
