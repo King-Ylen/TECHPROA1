c2c458a43d0e1e61f147a535f16095230ec92309
king <kingylen.scc@gmail.com>
2025-10-24 13:50:16 +0800

    done

--- SeatworkFourteen.java
+++ SeatworkFourteen.java
@@ -1,41 +1,47 @@
-class Motorcycle {
-    String make;
-    String color;
-    boolean engineState = false;
-
-    void showAtts() {
-        System.out.println("Make: " + make);
-        System.out.println("Color: " + color);
-        System.out.println("Engine state: " + (engineState ? "On" : "Off"));
-    }
-
-    void startEngine() {
-        if (engineState == true) {
-            System.out.println("The engine is already on.");
-        } else {
-            engineState = true;
-            System.out.println("The engine is now on.");
-        }
-    }
-}
+import java.util.Scanner;
 
 public class SeatworkFourteen {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        class Motorcycle {
+            String make;
+            String color;
+            boolean engineState = false;
+
+            void starEngine() {
+                if(engineState) {
+                    System.out.println("The engine is already on.");
+                } else {
+                    System.out.println("The engine is now on.");
+                    engineState = true;
+                }
+            }
+
+            void showAtts() {
+                System.out.println("This mototrcycle is a " + color + " " + make);
+                System.out.println("Engine State: " + (engineState ? "on" : "off"));
+            }
+        }
+
         Motorcycle m = new Motorcycle();
-        m.make = "Yamaha RZ350";
-        m.color = "Yellow";
+        m.make = "kawasaki 1000cc";
+        m.color = "blue";
+        
+        System.out.println("Type 'on' to start the engine, or 'show' to view attributes: ");
+        System.out.println("engine state is off.");
+        System.out.print("Command: ");
+        String command = scnr.nextLine();
 
-        System.out.println("Calling showAtts...");
-        m.showAtts();
-        System.out.println("--------");
-        System.out.println("Starting engine...");
-        m.startEngine();
-        System.out.println("--------");
+        if (command .equalsIgnoreCase("on")) {
+            System.out.println("Engine is starting...");
+            m.starEngine();
+        } else if (command.equalsIgnoreCase("show")) {
+            m.showAtts();
+        } else {
+            System.out.println("Unknown command.");
+        }
 
-        System.out.println("Calling showAtts...");
-        m.showAtts();
-        System.out.println("--------");
-        System.out.println("Starting engine...");
-        m.startEngine();
+        scnr.close();
     }
 }
