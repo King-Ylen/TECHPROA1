c3b1e6ca08f7e03d4d9d39d089d4abd416b46dbb
king <kingylen.scc@gmail.com>
2025-10-24 14:02:41 +0800

    done

---
+++ SeatworkFifthteenP2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SeatworkFifthteenP2 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Enter your score: ");
+        double score = scnr.nextDouble();
+
+        if (score >= 90.1) {
+            System.out.println("Your grade is A!!");
+        } else if (score >= 80.1) {
+            System.out.println("Your grade is B!!");
+        } else if (score >= 70.1) {
+            System.out.println("Your grade is C!!");
+        } else if (score >= 60.1) {
+            System.out.println("Your grade is D!!");
+        } else {
+            System.out.println("Your grade is F!! Failed");
+        }
+    } 
+}
